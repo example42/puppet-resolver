f23157e2780fafa8f67fd76de91a664cd8857c43
Martin Alfke <ma@example42.com>
2015-08-10 17:39:23 +0200

    rspec puppet 4

--- spec/classes/resolver_spec.rb
+++ spec/classes/resolver_spec.rb
@@ -7,40 +7,35 @@ describe 'resolver' do
   let(:facts) { { :ipaddress => '10.42.42.42', :domain => 'test.dom' } }
 
   describe 'Test standard installation' do
-    it { should contain_file('resolv.conf').with_ensure('present') }
+    it { should contain_file('resolv.conf').with_ensure('file') }
     it 'should not touch existing file' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should be_nil
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should be_nil
+      should contain_file('resolv.conf').with_content(nil)
+      should contain_file('resolv.conf').with_source(nil)
     end
   end
 
   describe 'Test standard installation with monitoring and firewalling' do
     let(:params) { {:monitor => true , :firewall => true } }
 
-    it { should contain_file('resolv.conf').with_ensure('present') }
+    it { should contain_file('resolv.conf').with_ensure('file') }
     it 'should place a firewall rule' do
-      content = catalogue.resource('firewall', 'resolver_udp_53').send(:parameters)[:enable]
-      content.should == true
+      should contain_firewall('resolver_udp_53').with_enable(true)
     end
   end
 
   describe 'Test with default domain' do
     let(:params) { { :dns_servers => ['nameserver1', 'nameserver2'] } }
 
-    it { should contain_file('resolv.conf').with_ensure('present') }
+    it { should contain_file('resolv.conf').with_ensure('file') }
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "domain test.dom"
-      content.should match "nameserver nameserver1"
-      content.should match "nameserver nameserver2"
-      content.should_not match "sortlist"
-      content.should_not match "options"
+      should contain_file('resolv.conf').with ({
+        'content' => /domain test.com/,
+        'content' => /nameserver nameserver1/,
+        'content' => /nameserver nameserver2/
+      })
     end
     it 'should not request a source' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should be_nil
+      should contain_file('resolv.conf').with_source(nil)
     end
   end
 
@@ -48,17 +43,17 @@ describe 'resolver' do
     let(:params) { {:dns_domain => 'the_domain', :search => ['search1', 'search2'], :dns_servers => ['nameserver1', 'nameserver2'], :sortlist => ['sort1', 'sort2'] } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "domain the_domain"
-      content.should match "search search1 search2"
-      content.should match "nameserver nameserver1"
-      content.should match "nameserver nameserver2"
-      content.should match "sortlist sort1 sort2"
-      content.should_not match "options"
+      should contain_file('resolv.conf').with ({
+        'content' => /domain the_domain/,
+        'content' => /search search1 search2/,
+        'content' => /nameserver nameserver1/,
+        'content' => /nameserver nameserver2/,
+        'content' => /sortlist sort1 sort2/
+      })
+      should contain_file('resolv.conf').without_content(/options/)
     end
     it 'should not request a source' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should be_nil
+      should contain_file('resolv.conf').with_source(nil)
     end
   end
 
@@ -66,17 +61,17 @@ describe 'resolver' do
     let(:params) { {:dns_domain => 'the_domain', :search => 'search1,search2', :dns_servers => 'nameserver1,nameserver2', :sortlist => 'sort1,sort2' } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "domain the_domain"
-      content.should match "search search1 search2"
-      content.should match "nameserver nameserver1"
-      content.should match "nameserver nameserver2"
-      content.should match "sortlist sort1 sort2"
-      content.should_not match "options"
+      should contain_file('resolv.conf').with ({
+        'content' => /domain the_domain/,
+        'content' => /nameserver nameserver1/,
+        'content' => /nameserver nameserver2/,
+        'content' => /search search1 search2/,
+        'content' => /sortlist sort1 sort2/
+      })
+      should contain_file('resolv.conf').without_content(/options/)
     end
     it 'should not request a source' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should be_nil
+      should contain_file('resolv.conf').with_source(nil)
     end
   end
 
@@ -84,18 +79,20 @@ describe 'resolver' do
     let(:params) { {:dns_domain => 'the_domain', :dns_servers => 'server1', :options => { 'opt_a' => 'value_a', 'opt_b' => 'value_b', 'opt_c' => '' } } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "domain the_domain"
-      content.should match "nameserver server1"
-      content.should match "options opt_a:value_a"
-      content.should match "options opt_b:value_b"
-      content.should match "options opt_c"
-      content.should_not match "search"
-      content.should_not match "sortlist"
+      should contain_file('resolv.conf').with ({
+        'content' => /domain the_domain/,
+        'content' => /nameserver server1/,
+        'content' => /options opt_a:value_a/,
+        'content' => /options opt_b:value_b/,
+        'content' => /options opt_c/
+      })
+      should contain_file('resolv.conf').without ({
+          'content' => /search/,
+          'content' => /sortlist/
+      })
     end
     it 'should not request a source' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should be_nil
+      should contain_file('resolv.conf').with_source(nil)
     end
   end
 
@@ -103,16 +100,13 @@ describe 'resolver' do
     let(:params) { {:template => "resolver/spec.erb" , :options => { 'opt_a' => 'value_a' } } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('resolv.conf').with_content(/fqdn: rspec.example42.com/)
     end
     it 'should generate a template that uses custom options' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "value_a"
+      should contain_file('resolv.conf').with_content(/value_a/)
     end
     it 'should not request a source' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should be_nil
+      should contain_file('resolv.conf').with_source(nil)
     end
   end
 
@@ -120,20 +114,17 @@ describe 'resolver' do
     let(:params) { { :source => "puppet://modules/resolver/spec" } }
 
     it 'should request a valid source' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:source]
-      content.should == "puppet://modules/resolver/spec"
+      should contain_file('resolv.conf').with_source("puppet://modules/resolver/spec")
     end
     it 'should not build template' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should be_nil
+      should contain_file('resolv.conf').with_content(nil)
     end
   end
 
   describe 'Test customizations - custom class' do
     let(:params) { {:my_class => "resolver::spec" } }
     it 'should automatically include a custom class' do
-      content = catalogue.resource('file', 'resolv.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('resolv.conf').with_content(/fqdn: rspec.example42.com/)
     end
   end
 
@@ -141,8 +132,7 @@ describe 'resolver' do
     let(:params) { {:puppi => true, :puppi_helper => "myhelper"} }
 
     it 'should generate a puppi::ze define' do
-      content = catalogue.resource('puppi::ze', 'resolver').send(:parameters)[:helper]
-      content.should == "myhelper"
+      should contain_puppi__ze('resolver').with_helper("myhelper")
     end
   end
 
